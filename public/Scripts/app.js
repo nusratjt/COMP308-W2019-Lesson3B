a3653525577f71140a8147b6911f7484e89ea36f
Your Name <you@example.com>
2019-02-02 01:43:59 -0500

    Added bootstrap

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,11 @@
-/* Custom JavaScript goes Here */
+/* IIFE -- Immediately INvoked Function Expression */
+(function(){
+
+    function Start(){
+        console.log(`%c App Started...`, 
+        "font-size: 20px; color: blue; font-weight: bold");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
